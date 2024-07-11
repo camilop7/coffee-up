0e88fc0d8c06329c6dc334513322bf69feccac3c
Camilo Palacios <camilop7@icloud.com>
2024-07-11 19:37:29 +0100

    added login.js

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+// src/components/Login.js
+
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      {/* Your login form or component JSX here */}
+      <h2>Login Component</h2>
+    </div>
+  );
+};
+
+export default Login;
